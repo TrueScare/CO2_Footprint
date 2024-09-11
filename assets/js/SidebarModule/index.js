8a2de37a35e33fbcac8186ca46988715f480cbd1
Liam Mefus <liam.mefus@outlook.de>
2024-09-11 17:27:50 +0200

    feat(sidebar,styling): load content in sidebar and add more styling

--- assets/js/SidebarModule/index.js
+++ assets/js/SidebarModule/index.js
@@ -1,5 +1,7 @@
 import SidebarHandler from "./SidebarHandler.js";
+import TableOfContentBuilder from "./TableOfContentBuilder.js";
 
 document.addEventListener('DOMContentLoaded', ()=>{
    let sidebarHandler = new SidebarHandler("#navigation");
+   let tableOfContentBuilder = new TableOfContentBuilder("#sidebar_content")
 });
