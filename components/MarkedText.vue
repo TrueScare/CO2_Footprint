111da5550402e653a31cb63910eae80501512ab9
Liam Mefus <liam.mefus@outlook.de>
2024-09-14 21:52:19 +0200

    feature(marked-content): introduce separate component for highlighted content

---
+++ components/MarkedText.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  content: String,
+  filterString: String
+});
+
+const markedContent = computed(() => {
+  let searchIn = props.content.toLowerCase();
+  let position = searchIn.indexOf(props.filterString.toLowerCase());
+  if (position >= 0 && props.filterString.length > 0) {
+    let exchange = props.content.substring(position, position + props.filterString.length);
+    return props.content.replace(exchange, "<mark>" + exchange + "</mark>");
+  } else {
+    return props.content;
+  }
+});
+
+</script>
+<template>
+  <span v-html="markedContent"></span>
+</template>
