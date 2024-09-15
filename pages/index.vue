40a30431317219b9a858910c962ed972e8c43400
Liam Mefus <liam.mefus@outlook.de>
2024-09-15 21:49:29 +0200

    fix(layout): add container class to content section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <section id="mission" aria-labelledby="misison_headline">
+
+  <section class="container" id="mission" aria-labelledby="misison_headline">
     <h2 id="misison_headline">Unsere Mission</h2>
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
     dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
