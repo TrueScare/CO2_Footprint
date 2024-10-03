72c9c9793716fa00cf48191cffdea6e23099e4fc
Liam Mefus <liam.mefus@outlook.de>
2024-10-03 11:01:53 +0200

    move(page_contents): rename content components dir
    clashed with the contents module which resulted in faulty page loads

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,21 @@
 <template>
-  <ContentSection title="Unsere Mission" id="mission">
+  <FillsSection title="Unsere Mission" id="mission">
     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
     dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
     clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
     consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
     diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
     takimata sanctus est Lorem ipsum dolor sit amet.
-  </ContentSection>
+  </FillsSection>
 
-  <ContentSection title="CO₂ Fußabdruck" id="co2_tabelle">
+  <FillsSection title="CO₂ Fußabdruck" id="co2_tabelle">
     <CO2Table data-url="countries"/>
-  </ContentSection>
+  </FillsSection>
 
-  <ContentSection title="Sonstiges" id="sonstiges">
-    <ContentCardImageText title="Du willst mitmachen?" src="/img/liam.jpg" alt="Liam, ziemlich happy!">
+  <FillsSection title="Sonstiges" id="sonstiges">
+    <FillsCardImageText title="Du willst mitmachen?" src="/img/liam.jpg" alt="Liam, ziemlich happy!">
       Super dann komm doch in unser Team. Die Kontaktdaten findest du im
       <NuxtLink to="/impressum">Impressum</NuxtLink>.
-    </ContentCardImageText>
-  </ContentSection>
+    </FillsCardImageText>
+  </FillsSection>
 </template>
-<script setup lang="js">
-</script>
